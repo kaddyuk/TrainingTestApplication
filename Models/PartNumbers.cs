23e4f0d42c004d5cce8c6e7558aa00d163cc32af
Kevin Cadd <kevin.cadd@rusada.com>
2021-11-17 16:37:13 +0000

    Day One Changes

--- Models/PartNumbers.cs
+++ Models/PartNumbers.cs
@@ -5,10 +5,11 @@ namespace TrainingTestApplication.Models
     {
         public int ID { get; init; }
         public string PartNo { get; init; } = string.Empty;
-        public string PartDescription { get; init;} = string.Empty;
+        public string PartDescription { get; set; } = string.Empty;
+        public string? Model { get; init; }
         public string PartClassification { get; init; } = string.Empty;
         public bool? IsRotable { get; init; }
-        public string? UnitOfMeasure { get; init; } = string.Empty;
+        public string UnitOfMeasure { get; init; } = string.Empty;
         public int StockCount { get; init; }
         public DateTime RecordTimeStampCreated { get; init; }
 
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,17 +1,39 @@
 ﻿using EnvisionClient.Data.Contexts;
 using EnvisionClient.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Data;
 
 namespace TrainingTestApplication.ViewModels;
 
 public class MainWindowViewModel : INotifyPropertyChanged
 {
     private readonly IDbContextFactory<TechRecordsDbContext> _dbContextFactory;
+    private string filterString = string.Empty;
+    private ICollectionView? partNumberCollectionView;
 
     public MainWindowViewModel(IDbContextFactory<TechRecordsDbContext> dbContextFactory)
     {
         _dbContextFactory = dbContextFactory;
+
+        NotifyPartNumberList = new NotifyTaskCompletion<List<PartNumber>>(GetPartNumbers());
+        NotifyPartNumberList.PropertyChanged += NotifyPartNumberList_PropertyChanged;
+    }
+
+    private void NotifyPartNumberList_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (!NotifyPartNumberList.IsSuccessfullyCompleted) return;
+
+        if (PartNumberCollectionView != null) return;
+
+        PartNumberCollectionView = CollectionViewSource.GetDefaultView(NotifyPartNumberList.Result);
+        PartNumberCollectionView.Filter = Filter;
+
+        if(PartNumberCollectionView.CanGroup)
+        {
+            PartNumberCollectionView.GroupDescriptions.Add(new PropertyGroupDescription(nameof(PartNumber.Model)));
+        }
     }
 
     /// <summary>
@@ -61,4 +83,68 @@ public class MainWindowViewModel : INotifyPropertyChanged
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
+
+    /// <summary>
+    /// Get a list of part numbers from the Database Asynchronously
+    /// </summary>
+    /// <returns></returns>
+    public async Task<List<PartNumber>> GetPartNumbers()
+    {
+        await using (var context = _dbContextFactory.Create())
+        {
+            return await (from sp in context.sPart
+                          join pc in context.sPartClassification on sp.sPartClassification_ID equals pc.ID
+                          join uom in context.sPartUnit on sp.sPartUnit_ID equals uom.ID
+                          join un in context.sPartUnitName on uom.sPartUnitName_ID equals un.ID
+                          join tmp in context.tModelPart on sp.ID equals tmp.sPart_ID into _tmp
+                          from tmp in _tmp.DefaultIfEmpty()
+                          join tm in context.tModel on tmp.tModel_ID equals tm.ID into _tm
+                          from tm in _tm.DefaultIfEmpty()
+                          select new PartNumber
+                          {
+                              ID = sp.ID,
+                              PartNo = sp.PartNo,
+                              PartDescription = sp.Description,
+                              Model = tm.Model,
+                              IsRotable = pc.Asset,
+                              PartClassification = pc.Description,
+                              RecordTimeStampCreated = sp.RecordTimeStampCreated,
+                              UnitOfMeasure = $"{un.Unit} {uom.UnitQty:0.00}",
+                          }).ToListAsync();
+        }
+    }
+
+    public NotifyTaskCompletion<List<PartNumber>> NotifyPartNumberList { get; set; }
+
+    public string FilterString { get => filterString; 
+        set => SetProperty(ref filterString, value, onChanged: FilterStringOnChanged); }
+
+    private void FilterStringOnChanged()
+    {
+        PartNumberCollectionView?.Refresh();
+    }
+
+    public Predicate<object> Filter
+    {
+
+        get
+        {
+            return x =>
+            {
+                if (x is not PartNumber part) return false;
+
+                switch (part)
+                {
+                    case var _ when part.PartNo.Contains(FilterString, StringComparison.CurrentCultureIgnoreCase):
+                    case var _ when part.PartDescription.Contains(FilterString, StringComparison.CurrentCultureIgnoreCase):
+                    case var _ when part.PartClassification.Contains(FilterString, StringComparison.CurrentCultureIgnoreCase):
+                        return true;
+                    default:
+                        return false;
+                }
+            };
+        }
+    }
+
+    public ICollectionView? PartNumberCollectionView { get => partNumberCollectionView; set => SetProperty(ref partNumberCollectionView, value); }
 }
